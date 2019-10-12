cb91721c87ef86902371b754bdef1b081bfa53b2
Pavel Sidyakin <fox.spa@gmail.com>
2019-10-12 20:26:43 +0300

    add calc distance stub

--- app/src/main/java/com/nearbyvenues/data/LocationRepositoryImpl.kt
+++ app/src/main/java/com/nearbyvenues/data/LocationRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.nearbyvenues.data
 
+import android.location.Location
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.nearbyvenues.domain.data.ApplicationProvider
@@ -16,6 +17,18 @@ class LocationRepositoryImpl
     constructor(
         private val applicationProvider: ApplicationProvider
     ) : LocationRepository {
+
+    override suspend fun calcDistanceBetweenCoordinates(point1: Coordinates, point2: Coordinates): Double {
+        // TODO: calc with distance matrix api:
+        // https://developers.google.com/maps/documentation/distance-matrix/intro#DistanceMatrixRequests
+        // Temporary calc
+
+        val results = floatArrayOf()
+
+        Location.distanceBetween(point1.lat, point1.lng, point2.lat, point2.lng, results)
+
+        return results[0].toDouble()
+    }
 
     private val fusedLocationClient: FusedLocationProviderClient by lazy {
         LocationServices.getFusedLocationProviderClient(applicationProvider.applicationContext)
--- app/src/main/java/com/nearbyvenues/domain/data/LocationRepository.kt
+++ app/src/main/java/com/nearbyvenues/domain/data/LocationRepository.kt
@@ -5,4 +5,6 @@ import com.nearbyvenues.model.Coordinates
 interface LocationRepository {
 
     suspend fun getLastLocation(): Coordinates?
+
+    suspend fun calcDistanceBetweenCoordinates(point1: Coordinates, point2: Coordinates): Double
 }
